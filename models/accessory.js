8bb987e2d23bec218c47658402bcd08f217849df
Andrew <mullinsa428@gmail.com>
2023-06-06 17:20:18 -0400

    accessory Schema added

--- models/accessory.js
+++ models/accessory.js
@@ -1,2 +1,15 @@
-const  { Schema } = require('mongoose')
+const { Schema } = require("mongoose");
 
+const accessorySchema = new Schema(
+    {
+        type: { type: String, require: true },
+        brand: { type: String, require: true },
+        item: { type: String, require: true },
+        price: { type: Number, require: true },
+        img: { type: String, require: true },
+        description: { type: String, require: true },
+    },
+    { timestamps: true }
+);
+
+module.exports = accessorySchema;
