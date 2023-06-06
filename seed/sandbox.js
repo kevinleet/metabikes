315c078a4f8ed49cdacd07e059fbb781cf5dde2a
Andrew <mullinsa428@gmail.com>
2023-06-06 17:54:52 -0400

    added sandbox with the accessories seed data. also addd accessory model information to models/index.js

---
+++ seed/sandbox.js
@@ -0,0 +1,29 @@
+const accessories = [
+    {
+        type: "Hybrid",
+        brand: "DRCKHROS",
+        item: "Handlebar Mirror",
+        price: 15.99,
+        img: "https://m.media-amazon.com/images/I/51SeeZL+ecL._AC_SL1000_.jpg",
+        description:
+            "Bicycle Rear View Mirror: Acrylic CONVEX mirror,diameter 3.23 inch. Inlaid In Black Plastic Shell. Can Adjust Diferent Mirror Angle.",
+    },
+    {
+        type: "Mountain",
+        brand: "SKS GERMANY",
+        item: "Fender Set",
+        price: 26.99,
+        img: "https://m.media-amazon.com/images/I/71BawVOWYuS._AC_SL1500_.jpg",
+        description:
+            "20 or 24 inche wheel compatible. Durable plastic construction / Metal and plastic harware. Rear mounts onto the seatpost of bike. 205 grams. Rear fender can pivot to adjust coverage.",
+    },
+    {
+        type: "Road",
+        brand: "BV",
+        item: "Bicycle Seat Pack bag",
+        price: 18.99,
+        img: "https://m.media-amazon.com/images/I/61zWI0mXarL._AC_SL1000_.jpg",
+        description:
+            "Big zippered opening mouth design for riders' convenience. Taillight hanger with 3M ScotchliteTM reflective trim. Capacious for your tire tools & personal belonging - Mini/ Co2 pump/ Levers/ Tube/ Key & Phone. Multiple sizes option from EXTRA Small to Large for all kinds of cycling activities",
+    },
+];
