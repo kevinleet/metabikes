07e43106b9a1d93e09e77cda3af805faa3afc640
Andrew <mullinsa428@gmail.com>
2023-06-07 09:55:33 -0400

    added a comment schema to the models and fed that into the index.js file

--- models/index.js
+++ models/index.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const bikeSchema = require("./bike");
 const accessorySchema = require("./accessory");
+const commentSchema = require("./comment");
 
 const Bike = mongoose.model("Bike", bikeSchema);
 const Accessory = mongoose.model("Accessory", accessorySchema);
+const Comment = mongoose.model("Comment", commentSchema);
 
-module.exports = { Bike, Accessory };
+module.exports = { Bike, Accessory, Comment };
