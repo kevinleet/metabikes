d7e0272aad3d78de2b1593f4e9183ba76bf7e167
Dan Mulka <danielmulka92@gmail.com>
2023-06-07 10:08:25 -0500

    morebikes

--- models/accessory.js
+++ models/accessory.js
@@ -2,7 +2,7 @@ const { Schema } = require("mongoose");
 
 const accessorySchema = new Schema(
     {
-        type: { type: Schema.Types.ObjectId, ref: 'Bike'},
+        type: { type: String, require: true },
         brand: { type: String, require: true },
         item: { type: String, require: true },
         price: { type: Number, require: true },
