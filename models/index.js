ebe5c9f8a658ed1d139f65c5b4e11ec595dac8e9
Andrew <mullinsa428@gmail.com>
2023-06-07 11:59:57 -0400

    fixed all cart variable names to cartItem

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const bikeSchema = require("./bike");
 const accessorySchema = require("./accessory");
 const commentSchema = require("./comment");
-const cartSchema = require("./cart");
+const cartItemSchema = require("./cartItem");
 
 const Bike = mongoose.model("Bike", bikeSchema);
 const Accessory = mongoose.model("Accessory", accessorySchema);
