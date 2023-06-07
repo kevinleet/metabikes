b597be77fff033a87dc141b4f9687cca21ed036f
Dan Mulka <danielmulka92@gmail.com>
2023-06-07 13:05:04 -0500

    moreAccessories

--- seed/sandbox.js
+++ seed/sandbox.js
@@ -1,29 +1,47 @@
 const accessories = [
     {
-        type: "Hybrid",
-        brand: "DRCKHROS",
-        item: "Handlebar Mirror",
-        price: 15.99,
-        img: "https://m.media-amazon.com/images/I/51SeeZL+ecL._AC_SL1000_.jpg",
+
+        type: "Road",
+        brand: "Flux",
+        item: "Taillight",
+        price: 55.99,
+        img: "https://assets.specialized.com/i/specialized/49120-250_LGHT_FLUX-250R-TAILLIGHT_DETAIL-1?bg=rgb(241,241,241)&w=2500&h=1406&fmt=auto",
         description:
-            "Bicycle Rear View Mirror: Acrylic CONVEX mirror,diameter 3.23 inch. Inlaid In Black Plastic Shell. Can Adjust Diferent Mirror Angle.",
+            "Serious road riders put in serious miles. Most of those miles occur in the daytime, when bright ambient conditions mean you need huge lumens numbers to be seen by motorists.But there's no reason a super-bright taillight can't look the part on the fastest road bikes. You invest in the best road bike technology--you deserve an equally fast taillight.Packing a huge punch with 250 lumens, the Flux 250R taillight is visible at more than 1 mile (1.5 km) in the brightest daytime conditions. Here in Morgan Hill, California we get those conditions all the time—this is the taillight we made for ourselves, and for you.",
+            category: "accesory"
     },
     {
-        type: "Mountain",
-        brand: "SKS GERMANY",
-        item: "Fender Set",
-        price: 26.99,
-        img: "https://m.media-amazon.com/images/I/71BawVOWYuS._AC_SL1500_.jpg",
+
+        type: "Road",
+        brand: "Flux",
+        item: "Headlight",
+        price: 104.99,
+        img: "https://assets.specialized.com/i/specialized/49119-161_LGHT_FLUX-1200-HEADLIGHT_MOUNT?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto",
         description:
-            "20 or 24 inche wheel compatible. Durable plastic construction / Metal and plastic harware. Rear mounts onto the seatpost of bike. 205 grams. Rear fender can pivot to adjust coverage.",
+            "The newest top-of-the-line Flux headlight continues the Flux tradition of optimal beam pattern, wide variety of mounting options, and super rugged construction. Compared to the Flux 1200, the Flux 1250 increases lumens and runtime, throws in the helmet and camera-style mounts... BOOM! Mode Shift user interface gives you three steady modes for just the right mix of lumens and runtimes to match riding conditions. Press-and-hold to enter the secondary modes, where you will find a Steady Flash mode for increased visibility in urban conditions, a Flash mode for daytime use, and 10 and 20 hour flashlight modes for bikepacking conditions, or when you forget to recharge and need to maximize the last few percent of battery life. IPX7 waterproof means this light can survive being submerged 1 meter deep for 30 minutes. No, it's not meant for scuba diving. But is can handle anything you can throw at it on two wheels.",
+            category: "accessory"
+            
     },
+    {
+
+        type: "Road",
+        brand: "Elite",
+        item: "Rear Rack",
+        price: 44.99,
+        img: "https://assets.specialized.com/i/specialized/9892-5025_CMPNT_ELITE-RACK_BLK?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto",
+        description: "The Elite Rear Rack is a versatile rack that allows you to carry your gear wherever you go. It features a 25kg (55lbs) max load capacity, lightweight 6061 alloy construction, and fast, easy installation.",
+        category: "accessory"
+    }, 
     {
         type: "Road",
-        brand: "BV",
-        item: "Bicycle Seat Pack bag",
-        price: 18.99,
-        img: "https://m.media-amazon.com/images/I/61zWI0mXarL._AC_SL1000_.jpg",
-        description:
-            "Big zippered opening mouth design for riders' convenience. Taillight hanger with 3M ScotchliteTM reflective trim. Capacious for your tire tools & personal belonging - Mini/ Co2 pump/ Levers/ Tube/ Key & Phone. Multiple sizes option from EXTRA Small to Large for all kinds of cycling activities",
+        brand: "Elite",
+        item: "Rear Rack",
+        price: 44.99,
+        img: "https://assets.specialized.com/i/specialized/9892-5025_CMPNT_ELITE-RACK_BLK?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto",
+        description: "The Elite Rear Rack is a versatile rack that allows you to carry your gear wherever you go. It features a 25kg (55lbs) max load capacity, lightweight 6061 alloy construction, and fast, easy installation.",
+        category: "accessory"
+
     },
+
+
 ];
