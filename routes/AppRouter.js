40144f15c927be93045f69e1e7d8433939d9b5e9
Andrew <mullinsa428@gmail.com>
2023-06-07 09:52:50 -0400

    added a commentRouter to work with the commentController. Added that router into the AppRouter script.

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -3,8 +3,10 @@ const Router = express.Router();
 
 const BikeRouter = require("./bikeRouter");
 const AccesoryRouter = require("./accessoryRouter");
+const CommentRouter = require("./commentRouter");
 
 Router.use("/bikes", BikeRouter);
 Router.use("/accessories", AccesoryRouter);
+Router.use("/comments", CommentRouter);
 
 module.exports = Router;
