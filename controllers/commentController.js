7f975505be6a8305186be65f60acafbccf3feb69
Andrew <mullinsa428@gmail.com>
2023-06-07 10:27:14 -0400

    added created, update, delete and additional sorting options to the accessoryController. Changed some minor variable names in the other two controllers

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -42,11 +42,11 @@ const createComment = async (req, res) => {
 const updateComment = async (req, res) => {
     try {
         const { id } = req.params;
-        const updateComment = await Comment.findByIdAndUpdate(id, req.body, {
+        const updatedComment = await Comment.findByIdAndUpdate(id, req.body, {
             new: true,
         });
-        if (updateComment) {
-            return res.json({ updateComment });
+        if (updatedComment) {
+            return res.json({ updatedComment });
         }
         return res.send("Oops.  No bike Found. You made Mark sad. :'(");
     } catch (e) {
@@ -58,9 +58,9 @@ const updateComment = async (req, res) => {
 const deleteComment = async (req, res) => {
     try {
         const { id } = req.params;
-        const deleteComment = await Comment.findByIdAndDelete(id);
-        if (deleteComment) {
-            return res.json({ deleteComment });
+        const deletedComment = await Comment.findByIdAndDelete(id);
+        if (deletedComment) {
+            return res.json({ deletedComment });
         }
         return res.send("Oops. No Bike found. You made Mark sad. :'(");
     } catch (e) {
