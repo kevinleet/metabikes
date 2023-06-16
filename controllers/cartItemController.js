c8b606c0d9c638ce1dee2857dd2600d7ea1fac90
Kevin Li <kevinli617@gmail.com>
2023-06-16 18:39:43 -0400

    update readme

--- controllers/cartItemController.js
+++ controllers/cartItemController.js
@@ -1,57 +1,59 @@
 const { CartItem } = require("../models");
-const someCRUD = require("../controllers/someCRUD");
+const someCRUD = require("./someCRUD");
 
 const getCartItems = async (req, res) => {
-    const cartItems = await CartItem.find({}).populate("bicycleID").populate("accessoryID");
-    res.json(cartItems);
+  const cartItems = await CartItem.find({})
+    .populate("bicycleID")
+    .populate("accessoryID");
+  res.json(cartItems);
 };
 
 const getCartItemsById = async (req, res) => {
-    someCRUD.getObjectById(req, res, CartItem);
+  someCRUD.getObjectById(req, res, CartItem);
 };
 
 const getCartItemByBicycleID = async (req, res) => {
-    try {
-        const { bicycleID } = req.params;
-        const cartItem = await CartItem.find({ bicycleID: bicycleID });
-        if (!cartItem) throw Error("Cart Object ItemID Not Found");
-        res.json(cartItem);
-    } catch (e) {
-        console.log(e);
-        res.send("Oops. That didn't work. You made Mark sad. :'(");
-    }
+  try {
+    const { bicycleID } = req.params;
+    const cartItem = await CartItem.find({ bicycleID: bicycleID });
+    if (!cartItem) throw Error("Cart Object ItemID Not Found");
+    res.json(cartItem);
+  } catch (e) {
+    console.log(e);
+    res.send("Oops. That didn't work. You made Mark sad. :'(");
+  }
 };
 
 const getCartItemByAccessoryID = async (req, res) => {
-    try {
-        const { accessoryID } = req.params;
-        const cartItem = await CartItem.find({ accessoryID: accessoryID });
-        if (!cartItem) throw Error("Cart Object ItemID Not Found");
-        res.json(cartItem);
-    } catch (e) {
-        console.log(e);
-        res.send("Oops. That didn't work. You made Mark sad. :'(");
-    }
+  try {
+    const { accessoryID } = req.params;
+    const cartItem = await CartItem.find({ accessoryID: accessoryID });
+    if (!cartItem) throw Error("Cart Object ItemID Not Found");
+    res.json(cartItem);
+  } catch (e) {
+    console.log(e);
+    res.send("Oops. That didn't work. You made Mark sad. :'(");
+  }
 };
 
 const createCartItem = async (req, res) => {
-    someCRUD.createObject(req, res, CartItem);
+  someCRUD.createObject(req, res, CartItem);
 };
 
 const updateCartItem = async (req, res) => {
-    someCRUD.updateObjectByID(req, res, CartItem);
+  someCRUD.updateObjectByID(req, res, CartItem);
 };
 
 const deleteCartItem = async (req, res) => {
-    someCRUD.deleteObjectByID(req, res, CartItem);
+  someCRUD.deleteObjectByID(req, res, CartItem);
 };
 
 module.exports = {
-    createCartItem,
-    getCartItems,
-    getCartItemsById,
-    updateCartItem,
-    deleteCartItem,
-    getCartItemByBicycleID,
-    getCartItemByAccessoryID,
+  createCartItem,
+  getCartItems,
+  getCartItemsById,
+  updateCartItem,
+  deleteCartItem,
+  getCartItemByBicycleID,
+  getCartItemByAccessoryID,
 };
